ae450370fdc45110ff349289a764007a29086fa4
Udara Seneviratne <udara@192.168.1.106>
2018-02-02 16:38:07 +0530

    upgarded to java 8, hibernate 5 and spring 4

--- buildNDeploy.sh
+++ buildNDeploy.sh
@@ -1,7 +1,7 @@
 source ~/.bash_profile
-rm -rf /home/udara/apps/apache-tomcat-7.0.42/webapps/multiple-dbs*
+rm -rf /home/udara/apps/apache-tomcat-8.5.5/webapps/multiple-dbs*
 mvn clean install -DskipTests
-cp ./rest/target/multiple-dbs.war /home/udara/apps/apache-tomcat-7.0.42/webapps/
+cp ./rest/target/multiple-dbs.war /home/udara/apps/apache-tomcat-8.5.5/webapps/
 export JPDA_ADDRESS=5005
 export JPDA_TRANSPORT=dt_socket
-/home/udara/apps/apache-tomcat-7.0.42/bin/catalina.sh jpda run
+/home/udara/apps/apache-tomcat-8.5.5/bin/catalina.sh jpda run
